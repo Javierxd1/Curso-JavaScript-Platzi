2ae0584e61c51fafc1bb075804ca15714efd2253
Javier Agudelo <javieragu1104@gmail.com>
2025-05-14 20:31:51 -0500

    Clase #2: Tipos de datos

---
+++ tipos_de_datos.js
@@ -0,0 +1,38 @@
+//Datos primitivos
+
+//Strings: Cadenas de caracteres
+let nombre = 'Javier'
+
+//Number: Datos numéricos
+let edad = 24
+
+//Boolean: Verdaderos o falsos
+let mayorDeEdad = true
+
+//Nulo: datos nulos, vacios
+let noValor = null
+
+//Undefined: Dato no definidos
+let noDefinido = undefined
+
+//Symbol: Indica el valor único
+let simboloUnico = Symbol('único')
+
+//BigInt: Entero con varios decimales
+let numeroGrande = 2n
+
+//Datos Complejos
+
+//Objeto:
+let carro = {
+    'model': 'Chevrolet Sail',
+    'year': 2005
+}
+
+//Arrays:
+let frutas = ['Manzana','Banana','Naranja']
+
+//Funciones: Deben llevar la palabra reservada function
+function saludar (name) {
+    console.log('Hola' , name)
+}
